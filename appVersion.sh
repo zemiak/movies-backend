218a79a8b5880059fda34e17f127e1da926642b2
Miroslav Vaško <vasko@Miroslavs-Mac-mini.local>
2019-05-16 17:15:06 +0200

    removed some not used stuff

--- appVersion.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cat pom.xml | grep '<version>' | head -1 | cut -d'>' -f2 | cut -d'<' -f1
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-quarkusVersion="$(sh quarkusVersion.sh)"
-appVersion="$(sh appVersion.sh)"
+quarkusVersion="0.15.0"
+appVersion="$(cat pom.xml | grep '<version>' | head -1 | cut -d'>' -f2 | cut -d'<' -f1)"
+artifactId="$(cat pom.xml | grep '<artifactId>' | head -1 | cut -d'>' -f2 | cut -d'<' -f1)"
+
 docker build --build-arg quarkusVersion=${quarkusVersion} --build-arg appVersion=${appVersion} \
-    -t movies-backend:${appVersion} .
+    -t ${artifactId}:${appVersion} .
