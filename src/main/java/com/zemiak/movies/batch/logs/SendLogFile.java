2384459d2b7873f44c8b3b0df157eef738cc8643
Miroslav Vaško <vasko@Miroslavs-Mac-mini.local>
2019-06-06 10:02:17 +0200

    Added mail sending via Quarkus Mail

--- src/main/java/com/zemiak/movies/batch/logs/SendLogFile.java
+++ src/main/java/com/zemiak/movies/batch/logs/SendLogFile.java
@@ -1,28 +1,31 @@
 package com.zemiak.movies.batch.logs;
 
 import java.io.File;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.Dependent;
-//import javax.mail.*;
-//import javax.mail.internet.InternetAddress;
-//import javax.mail.internet.MimeBodyPart;
-//import javax.mail.internet.MimeMessage;
-//import javax.mail.internet.MimeMultipart;
+import javax.inject.Inject;
 
 import com.zemiak.movies.lookup.ConfigurationProvider;
 
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
+
 @Dependent
 public class SendLogFile {
-    private static final Logger LOG = Logger.getLogger(SendLogFile.class.getName());
+    private static final String TEXT_PLAIN = "text/plain";
 
-//    @Resource(name = "mail/movies")
-//    private Session mailSession;
+    private static final Logger LOG = Logger.getLogger(SendLogFile.class.getName());
 
     private final String mailSubject = "Movies " + ConfigurationProvider.getSystemName() + ": Batch Results";
     private final String mailTo = ConfigurationProvider.getMailTo();
-    private final String mailFrom = "noreply@movies-" + ConfigurationProvider.getSystemName().toLowerCase() + ".zemiakbox.com";
+    private final String mailFrom = "noreply@movies-" + ConfigurationProvider.getSystemName().toLowerCase()
+            + ".zemiakbox.com";
+
+    @Inject
+    Mailer mailer;
 
     public void send() {
         if (ConfigurationProvider.isDevelopmentSystem()) {
@@ -30,7 +33,7 @@ public class SendLogFile {
         }
 
         final File file = new File(BatchLogger.getLogFileName());
-        if (! file.exists()) {
+        if (!file.exists()) {
             LOG.log(Level.INFO, "Log file does not exist, not sending...");
             return;
         }
@@ -48,24 +51,10 @@ public class SendLogFile {
     }
 
     private void sendLogFile() throws Exception {
-//        final Message message = new MimeMessage(mailSession);
-//        message.setFrom(new InternetAddress(mailFrom));
-//        message.setRecipient(Message.RecipientType.TO, new InternetAddress(mailTo));
-//        message.setSubject(mailSubject);
-//        message.setText("Batch run ended on " + new Date());
-//
-//        MimeBodyPart messageBodyPart = new MimeBodyPart();
-//        Multipart multipart = new MimeMultipart();
-//
-//        DataSource source = new FileDataSource(BatchLogger.getLogFileName());
-//        messageBodyPart.setDataHandler(new DataHandler(source));
-//        messageBodyPart.setFileName("batch.log");
-//        multipart.addBodyPart(messageBodyPart);
-//
-//        message.setContent(multipart);
-//
-//        Transport.send(message);
-//
-//        LOG.log(Level.INFO, "Sent LOG file to {0}", mailTo);
+        mailer.send(Mail
+            .withText(mailTo, mailSubject, "Batch run ended on " + new Date())
+            .setFrom(mailFrom)
+            .addAttachment("batch.log", new File(BatchLogger.getLogFileName()), TEXT_PLAIN)
+        );
     }
 }
