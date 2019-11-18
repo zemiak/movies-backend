37bcdff191727bf2ca91356f939a0a6e08b5182b
Miroslav Vasko <vasko@Miroslavs-Mac-mini.local>
2019-11-18 17:51:12 +0100

    not fabric8

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-quarkusVersion="$(cat pom.xml | grep '<quarkus.version>' | head -1 | cut -d'>' -f2 | cut -d'<' -f1)"
-appVersion="$(cat pom.xml | grep '<version>' | head -1 | cut -d'>' -f2 | cut -d'<' -f1)"
-artifactId="$(cat pom.xml | grep '<artifactId>' | head -1 | cut -d'>' -f2 | cut -d'<' -f1)"
 
-docker build --build-arg quarkusVersion=${quarkusVersion} --build-arg appVersion=${appVersion} \
-    -t ${artifactId}:${appVersion} .
+mvn clean package || exit 10
+docker build . -f ./src/main/docker/Dockerfile-jvm -t movies
