7a3730128c711de28066044191919d2b83774f6d
zemiak <zemiak@gmail.com>
2020-04-04 16:40:54 +0200

    warnings

--- src/test/java/com/zemiak/movies/metadata/ItunesArtworkServiceTest.java
+++ src/test/java/com/zemiak/movies/metadata/ItunesArtworkServiceTest.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 
