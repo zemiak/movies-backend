5b2f88922d03d41fe0854c8aa24111255b62cb6b
zemiak <zemiak@gmail.com>
2020-04-16 07:06:53 +0200

    Added a catch-all web filter  in Quarkus, redirects everything to index.html. Vaadin Router now works

--- src/main/resources/META-INF/resources/app.js
+++ src/main/resources/META-INF/resources/app.js
@@ -6,13 +6,12 @@ import { SerieView } from "./js/serie/SerieView.js";
 import { MovieView } from "./js/movie/MovieView.js";
 import { UnknownView } from "./js/unknown/UnknownView.js";
 import { AboutView } from "./js/about/AboutView.js";
-import { Router } from './js/lib/Router.js';
+import { Router } from './js/lib/@vaadin/Router.js';
 
-const outlet = document.querySelector('#view');
+const outlet = document.querySelector('#outlet');
 const router = new Router(outlet);
 router.setRoutes([
   {path: '/',     component: 'root-view'},
-  {path: '',     component: 'root-view'},
   {path: '/genre',  component: 'genre-view'},
   {path: '/serie',  component: 'serie-view'},
   {path: '/movie',  component: 'movie-view'},
