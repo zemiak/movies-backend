02d42f333860d96139925c3b4d4a77c2e20668ab
zemiak <zemiak@gmail.com>
2020-09-25 20:02:27 +0200

    install script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-tag=quay.io/zemiak/movies-backend
-
-mvn package -Dmaven.test.skip || exit 10
-docker build . -f ./src/main/docker/Dockerfile.fast-jar -t ${tag} || exit 20
-echo ${tag}
--- src/main/install/install.sh
+++ src/main/install/install.sh
@@ -3,17 +3,14 @@
 svc=movies
 
 which systemctl
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
 then
-    cp ./${svc}.service /etc/systemd/system/ || exit 10
-    systemctl daemon-reload || exit 20
-    systemctl enable ${svc}.service || exit 30
-    systemctl stop ${svc}.service
-    systemctl start ${svc}.service || exit 40
-else
-    cp ./${svc}.init /etc/init.d/${svc} || exit 10
-    chmod +x /etc/init.d/${svc}
-    /sbin/chkconfig --add ${svc} || exit 20
-    /sbin/service ${svc} stop
-    /sbin/service ${svc} start || exit 30
+    echo systemd system needed
+    exit 5
 fi
+
+cp ./${svc}.service /etc/systemd/system/ || exit 10
+systemctl daemon-reload || exit 20
+systemctl enable ${svc}.service || exit 30
+systemctl stop ${svc}.service
+systemctl start ${svc}.service || exit 40
